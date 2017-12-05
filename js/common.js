629d745c07f05395187d937c52f4471903c658b8
ArtemVoitenko <artvoitenk072@gmail.com>
2017-12-05 21:08:48 +0200

    fitness

--- js/common.js
+++ js/common.js
@@ -1,11 +1,44 @@
 $(function() {
-var $hovImg = $('.scaleImg');
-    $hovImg.hover(function(){
-    var $selectImg = $(this);
-    var $descr = $selectImg.siblings(".dPlace");
-        $descr.slideToggle();
-    console.log($descr);
-    });
-    
+	$('.topSlider').slick({
+		prevArrow: '<img src="img/prevArrow.png">',
+		nextArrow: '<img src="img/nextArrow.png">',
+		arrows: true,
+		useCss: true
+	});
+	$('.parthnersSlider').slick({
+		slidesToShow: 5,
+		prevArrow: '<img src="img/leftArrow2.png">',
+		nextArrow: '<img src="img/rightArrow2.png">',
+		arrows: true,
+		useCss: true,
+		infinite: true,
+		
+		responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                slidesToShow: 4,
+                }
+              },
+               {
+                breakpoint: 1024,
+                settings: {
+                slidesToShow: 3,
+                }
+              }
+                
+                   
+
+       
+           
+                    ]
+	});
+	var $showMnu = $(".showMnu");
+	var $nav = $(".mainNav")
+	$showMnu.click(function(e){
+		e.preventDefault();
+		$nav.slideToggle();
+
+
+	})
 });
-    
