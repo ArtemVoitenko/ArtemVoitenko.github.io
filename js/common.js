16dcdbacc1a9e4f65bf4576c73f2fce9a453dc57
ArtemVoitenko <artvoitenk072@gmail.com>
2017-12-05 22:06:34 +0200

    fitnesstime

--- js/common.js
+++ js/common.js
@@ -41,4 +41,12 @@ $(function() {
 
 
 	})
+	var $mnuLink = $(".mainNav a")
+	 $mnuLink.on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
 });
+
