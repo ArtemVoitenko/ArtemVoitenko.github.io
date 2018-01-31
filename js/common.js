16658a6f15397e13a2dd8ab524600da2117f8678
ArtemVoitenko <artvoitenk072@gmail.com>
2018-01-31 17:35:30 +0200

    pravoAgency

--- js/common.js
+++ js/common.js
@@ -1,78 +1,39 @@
-$(function() {
-	$('.topSlider').slick({
-		prevArrow: '<img src="img/prevArrow.png">',
-		nextArrow: '<img src="img/nextArrow.png">',
-		arrows: true,
-		useCss: true
-	});
-	$('.parthnersSlider').slick({
-		slidesToShow: 5,
-		prevArrow: '<img src="img/leftArrow2.png">',
-		nextArrow: '<img src="img/rightArrow2.png">',
-		arrows: true,
-		useCss: true,
-		infinite: true,
-		
-		responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                slidesToShow: 4,
-                }
-              },
-               {
-                breakpoint: 1024,
-                settings: {
-                slidesToShow: 3,
-                }
-              }
-                
-                   
+$(function(){
 
-       
-           
-                    ]
-	});
-	var $showMnu = $(".showMnu");
-	var $nav = $(".mainNav")
-	$showMnu.click(function(e){
+	var $menu = $('nav');
+	var $interestLink = $('.select a');
+	var $interestList = $('.check');
+	var $myFlag = false;
+	var $showMenu = $('.showMenu');
+	$interestLink.click(function(e){
+		if($myFlag) {
+			
+		 		$(this).css("backgroundImage", "url(img/angleDown.png)");
+		 		$myFlag = false;
+		 		}
+		 	else {
+		 		$(this).css("backgroundImage", "url(img/angleUp.png)");
+		 		$myFlag = true;
+		 	}
 		e.preventDefault();
-		$nav.slideToggle();
-
-
+		$interestList.slideToggle();
+		$('html, body').animate({
+      scrollTop: $interestLink.offset().top
+    }, 500);
 	});
-	var $mnuLink = $(".mainNav a")
-	 $mnuLink.on("click", function (event) {
+	$showMenu.click(function (e) {
+		e.preventDefault();
+		$menu.slideToggle();
+
+	})
+    var $menuLink = $(".mainMenu a");
+    $menuLink.on("click", function (event) {
         event.preventDefault();
-        var id  = $(this).attr('href'),
+        var id = $(this).attr('href'),
             top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
+        $('body,html').animate({
+            scrollTop: top
+        }, 1500);
     });
-
-	var $closeButton = $('.close');
-	var $modal = $('.modal');
-	$closeButton.click(function(){
-	$modal.hide();
-	});
-var $showModal = $('.showModal');
-var $modalImg = $('.modal img');
-var $modalImgSrc = $modalImg.attr('src');
-
- 
- $showModal.click(function(e){
- 	e.preventDefault();
- 	var $thsh = $(this);
- 	var $thImg=$thsh.next();
- 	var $ourSrc = $thImg.attr('src');
- 	$modalImg.attr("src" ,$ourSrc);
- 	var $showModPar = $(this).parent();
- 	var $descr = $showModPar.next();
- 	var $modalTitle = $descr.children('p').text();
- 	$modal.children("p").text($modalTitle);
- 	$modal.show();
-
- 	
-
- });
+	
 });
-
