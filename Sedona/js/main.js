36907c835e8155903e169a1a2e87bc4d8b4c4f8e
Artem <artVoitenk072@gmail.com>
2018-06-28 22:16:32 +0300

    AddSedona

---
+++ Sedona/js/main.js
@@ -0,0 +1,6 @@
+var showNavBtn = document.getElementsByClassName('main-nav__show');
+var mainNavItem = document.getElementsByClassName('main-nav__item');
+function show() {
+	console.log("hello");
+}
+showNavBtn.addEventListener("click", show);
